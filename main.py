f35d917ecad6a70825d36babd6f58daf476d2e63
vemolista <tomas@tomasvemola.com>
2025-05-19 11:57:06 +0000

    Rename function

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ actual_username = "hi"
 actual_password = "ho"
 
 
-def print_secret(username: str, password: str) -> bool:
+def is_valid_credentials(username: str, password: str) -> bool:
     if username == actual_username and password == actual_password:
         return True
 
 
-if print_secret(username, password):
+if is_valid_credentials(username, password):
     print("secret")
 else:
     print("get lost, fool!")
